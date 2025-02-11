c66141c4008e57a78d051cad96c6da967cb99e9b
ssjismi <160107907+ssjismi@users.noreply.github.com>
2025-02-11 22:20:56 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Product
+{
+     int pcode;
+     String pname;
+     int price;
+     public Product(int pcode,String pname,int price)
+     {
+       this.pcode=pcode;
+       this.pname=pname;
+       this.price=price;
+     }
+     void display()
+     {
+         System.out.println("Product code is "+pcode);
+         System.out.println("Product name is "+pname);
+         System.out.println("Product price is "+price);
+     }
+}
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter product code: ");
+        int pcode=sc.nextInt();
+        System.out.print("Enter product price: ");
+        int price=sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter product name: ");
+        String pname=sc.nextLine();
+        Product s=new Product(pcode,pname,price);
+        s.display();
+    }
+}
